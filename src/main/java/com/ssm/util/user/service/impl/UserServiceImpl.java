409e719b6a74885d2c7045c18e0dc59df4dbc4b3
MarvinChen <17554058@qq.com>
2017-09-18 22:30:34 +0800

    完善springmvc部分

--- src/main/java/com/ssm/util/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/ssm/util/user/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.ssm.util.user.service.impl;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ssm.util.user.IDao.IUserDao;
@@ -10,7 +11,8 @@ import com.ssm.util.user.service.IUserService;
 
 @Service("userService")
 public class UserServiceImpl implements IUserService{
-	@Resource
+	//@Resource
+	@Autowired
 	private IUserDao userDao;
 	
 	@Override
--- src/main/webapp/WEB-INF/jsp/showUser.jsp
+++ src/main/webapp/WEB-INF/jsp/showUser.jsp
@@ -8,4 +8,5 @@
   <body>
     ${user.userName}
   </body>
+  
 </html>
